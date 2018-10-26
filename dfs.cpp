1e008cabe43c7abc61499abef06452eb4bce95d8
neilvillamizar <neilvillamizar@gmail.com>
2018-10-25 22:12:46 -0400

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -2,10 +2,27 @@
 #include <iostream>
 #include <vector>
 
-int n,m;
-vector<int> g[200000];
+using namespace std;
 
-void dfs(int v){
+#define FOR(i,n,m) for(ll i=n; i<m; i++) //un #define es basicamente una sustitucion textual jeje para codear mas fast
+#define ROF(i,n,m) for(int i=n; i>m; i--)
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define F first
+#define S second
+#define ri(a) scanf("%d",&a)
+#define rii(a,b) scanf("%d %d",&a,&b)
+#define lri(a) scanf("%lld",&a)
+#define lrii(a,b) scanf("%lld %lld",&a,&b)
+#define ms(obj,val) memset(obj,val,sizeof(obj))
+#define debug true
+#define dprintf debug && printf
+
+int n,m; //n nodos y m aristas
+vector<int> g[200000]; //vector para guardar el grafo
+
+void dfs(int v){ // este es el que tal pino
   visit[v]=1;
   for(int u:g[v]){
     if(!visit[u]){
